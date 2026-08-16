a2b6faf876a9fcff3967eb91530ddf892242118e
Ville <ville-poikela@hotmail.com>
2026-08-16 20:21:33 +0300

    moves phonebook-backend to its own repository

---
+++ index.js
@@ -0,0 +1,120 @@
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
+const Person = require("./models/person")
+const app = express()
+
+app.use(express.static("dist"))
+app.use(express.json())
+app.use(cors())
+
+morgan.token("body", (req) => {
+  return JSON.stringify(req.body)
+})
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :body ",
+  ),
+)
+
+app.get("/api/people", (request, response) => {
+  Person.find({}).then((people) => {
+    response.json(people)
+  })
+})
+
+app.get("/api/people/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
+})
+
+app.get("/api/info", (request, response) => {
+  let count
+  Person.find({})
+    .then((people) => {
+      count = people.length
+    })
+    .then(() => {
+      response.send(
+        `<p>
+          Phonebook has info for ${count} people <br/>
+          ${new Date()}
+        </p>`,
+      )
+    })
+})
+
+app.delete("/api/people/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
+})
+
+app.post("/api/people", (request, response, next) => {
+  const body = request.body
+
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ error: "Contact info missing" })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson)
+    })
+    .catch((error) => next(error))
+})
+
+app.put("/api/people/:id", (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" },
+  )
+    .then((updatedPerson) => {
+      response.json(updatedPerson)
+    })
+    .catch((error) => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" })
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
